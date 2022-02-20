ca14dc9834efb2a3b3b516329d4ab688ef559a8d
ta-maehara <ta-maehara@kddi-tech.com>
2022-02-20 21:17:07 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taiki/maehara/calcapp/ResultCalcActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.taiki.maehara.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultCalcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_calc)
+    }
+}
